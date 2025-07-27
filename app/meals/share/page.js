e5c37b49f8a0e0f62da5fb870c99b5352034f6ad
Kahlil <kahlil12@hotmail.com>
2025-07-27 19:53:56 -0400

    externalizing meal form submission and server action

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,21 +1,10 @@
 import ImagePicker from '@/components/meals/image-picker';
 import classes from './page.module.css';
+import { shareMeal } from '@/lib/actions';
 
 export default function ShareMealPage() {
 
-    async function shareMeal(formData){
-        'use server';
-
-        const meal = {
-            title: formData.get('title'),
-            summary: formData.get('summary'),
-            instructions: formData.get('instructions'),
-            image: formData.get('image'),
-            creator: formData.get('name'),
-            creator_email: formData.get('email')
-        }
-
-    }
+    
 
   return (
     <>
@@ -26,7 +15,7 @@ export default function ShareMealPage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
+        <form className={classes.form} action={shareMeal}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
